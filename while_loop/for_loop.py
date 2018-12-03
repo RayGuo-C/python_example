ddabe05eb76bfd430147c8849434fb864bd9ada3
GuoRui <ray.guo.inchina@gmail.com>
2018-12-03 15:04:16 +0800

    loop

--- while_loop/for_loop.py
+++ while_loop/for_loop.py
@@ -6,3 +6,13 @@ for friend in friends:
 # another method
 for index in range(len(friends)):
     print(friends[index])
+
+for i in range(1,5):
+	print(i)
+else:
+	print('over')
+
+# to track the different collow
+a = [[[1,2,3,4],[2]],[[2,3,4,3],[2]],[[3,4,5,4],[2]]]
+for i in a:
+	print(i[0])
--- while_loop/while_loop.py
+++ while_loop/while_loop.py
@@ -16,3 +16,11 @@ while guess != secret_word and gameison:
 if guess == secret_word:
     print('you win')
 
+# while-while loop
+i = 0
+while i < 10:
+	j = 1 # otherwise the code cannot be implemented since the j = 11
+	while j < 11:
+	 	print(i, j)
+	 	j+=1
+	i+=1
