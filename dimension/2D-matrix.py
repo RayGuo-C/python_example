75e64ccbcd87dd22c97151bcf2b2053acdcc689d
GuoRui <ray.guo.inchina@gmail.com>
2019-01-12 21:33:25 +0800

    dimension

---
+++ dimension/2D-matrix.py
@@ -0,0 +1,13 @@
+import numpy as np
+n = np.arange(0.0, 1.0, 0.1)
+pickedCellCoord = [[1.0,0.8,0.2],[1.0,0.8,0.5],[1.0,0.8,0.8],[1.0,0.5,0.2],[1.0,0.5,0.5],[1.0,0.5,0.8],[1.0,0.2,0.2],[1.0,0.2,0.5],[1.0,0.2,0.8]] 
+for i in range((len(pickedCellCoord))):
+	if i<3:
+		for j in range(len(n)):
+			pickedCellCoord[i][0] = 
+			pickedCellCoord[i][2] = 
+	if 3<=i<6:
+		for j in range(len(n)):
+			pickedCellCoord[i][0] = n[j]
+			if distance(pickedCellCoord[i],vecPostion) < (r+0.1):
+				break
